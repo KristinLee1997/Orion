12a59c137657918db2f35c7f1126bf8df113a08d
Kristin <kristinleejava@gmail.com>
2019-04-29 10:19:21 +0800

    :tada: 更改配置

--- src/main/java/com/aries/orion/OrionApplication.java
+++ src/main/java/com/aries/orion/OrionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrionApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrionApplication.class, args);
     }
