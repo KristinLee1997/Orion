028b22dd3d4d2813059c4043ced09a9856b06726
Kristin <kristinleejava@gmail.com>
2019-05-01 11:36:14 +0800

    :sparkles: 更改页面样式

--- src/main/java/com/aries/orion/controller/UserController.java
+++ src/main/java/com/aries/orion/controller/UserController.java
@@ -7,20 +7,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @PostMapping("/register")
+    @ResponseBody
+    public String register(@RequestBody User user) {
+        System.out.println("后端注册被调用了=====");
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        return "{\"ok\":123}";
+    }
 
     @PostMapping("/login")
-    public void login(@RequestBody User user) {
-        System.out.println("后端被调用了=====");
-        return ;
+    @ResponseBody
+    public String login(@RequestBody User user) {
+        System.out.println("后端登录被调用了=====");
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        return "{\"ok\":123}";
     }
 
     @GetMapping("/test")
     public String test() {
         return "login";
     }
+
+
 }
